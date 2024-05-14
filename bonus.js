fa59728d20ce9b8685b41bba0685dc00e7ec6694
Kongooii <konglm41@gmail.com>
2024-05-14 16:24:08 +0700

    feat: bonus quiz

---
+++ bonus.js
@@ -0,0 +1,16 @@
+// find second maximum number
+function secondMax(nums) {
+  let firstMax = 0
+  let secondMax = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > firstMax) {
+      secondMax = firstMax
+      firstMax = nums[i]
+    } else if (nums[i] > secondMax && nums[i] !== firstMax) {
+      secondMax = nums[i]
+    }
+  }
+  return secondMax
+}
+
+console.log(secondMax([-1, 4, 30, 2, -4]))
